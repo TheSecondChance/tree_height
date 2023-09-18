7ebe6cae964bec8b798dd2f96f8b159003d5e0ff
TheSecondChance <132612647+TheSecondChance@users.noreply.github.com>
2023-09-18 18:24:29 +0400

    tree_shape.py

---
+++ tree_shape.py
@@ -0,0 +1,18 @@
+tree_l = int(input("Enter How Tree Long: "))
+
+space = tree_l - 1
+hash = 1
+f_space = tree_l - 1
+
+while tree_l != 0:
+    for i in range(space):
+        print(' ', end="")
+    for i in range(hash):
+        print('#', end="")
+    print()
+    space -= 1
+    hash += 2
+    tree_l -= 1
+for i in range(f_space):
+    print(' ', end="")
+print("#")
